6764389e90c3babe69d2ad318e6558fbdbb85a15
Holly Zbitowsky <hzbitowsky@yahoo.com>
2016-01-27 14:59:26 -0800

    update gemfile assests and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'homepages/index' => 'homepages#index'
-
-  get 'tests' => "tests#index"
   root 'landing#index'
-
+  get 'homepages/index' => 'homepages#index'
+  get 'organizations/earthjustice' => 'organizations#earth_justice'
+  get 'users/new' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
